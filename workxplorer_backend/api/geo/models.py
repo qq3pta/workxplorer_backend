5276e4d8ac1a0aa41591d3271ec4d10dbcc3a30c
Aleksandr <chatyev@inbox.ru>
2025-09-02 17:54:27 +0500

    postgis and files

---
+++ workxplorer_backend/api/geo/models.py
@@ -0,0 +1,21 @@
+from django.contrib.gis.db import models as gis_models
+from django.db import models
+
+class GeoPlace(models.Model):
+    name = models.CharField(max_length=128)
+    country = models.CharField(max_length=128)
+    country_code = models.CharField(max_length=2)
+    point = gis_models.PointField(geography=True, srid=4326)
+    provider = models.CharField(max_length=32, default="nominatim")
+    raw = models.JSONField(null=True, blank=True)
+    last_verified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = [("name", "country_code")]
+        indexes = [
+            gis_models.GISIndex(fields=["point"]),
+            models.Index(fields=["country_code", "name"]),
+        ]
+
+    def __str__(self):
+        return f"{self.name}, {self.country_code}"

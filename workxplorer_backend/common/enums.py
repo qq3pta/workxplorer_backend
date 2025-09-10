2d9f06cee524005079d2c08bfd6000fbf138f5b3
Aleksandr <chatyev@inbox.ru>
2025-09-10 10:28:59 +0500

    fix CI/CD

--- workxplorer_backend/common/enums.py
+++ workxplorer_backend/common/enums.py
@@ -6,13 +6,30 @@ class Role(StrEnum):
     CARRIER  = "CARRIER"
 
 class TransportType(StrEnum):
-    TENT="TENT"; CONT="CONTAINER"; REEF="REEFER"; DUMP="DUMP"; CARCAR="CAR_CARRIER"
-    GRAIN="GRAIN"; CRANE="CRANE"; TIMBER="TIMBER"; PICKUP="PICKUP"; CEM="CEMENT"
-    TANK="TANKER"; MEGA="MEGA"
+    TENT = "TENT"
+    CONT = "CONTAINER"
+    REEF = "REEFER"
+    DUMP = "DUMP"
+    CARCAR = "CAR_CARRIER"
+    GRAIN = "GRAIN"
+    CRANE = "CRANE"
+    TIMBER = "TIMBER"
+    PICKUP = "PICKUP"
+    CEM = "CEMENT"
+    TANK = "TANKER"
+    MEGA = "MEGA"
 
 class Currency(StrEnum):
-    UZS="UZS"; KZT="KZT"; RUB="RUB"; USD="USD"; EUR="EUR"
+    UZS = "UZS"
+    KZT = "KZT"
+    RUB = "RUB"
+    USD = "USD"
+    EUR = "EUR"
 
 class DocumentCategory(StrEnum):
-    LICENSE="LICENSE"; DRIVER="DRIVER"; CONTRACT="CONTRACT"
-    LOAD="LOAD"; UNLOAD="UNLOAD"; EXTRA="EXTRA"
+    LICENSE = "LICENSE"
+    DRIVER = "DRIVER"
+    CONTRACT = "CONTRACT"
+    LOAD = "LOAD"
+    UNLOAD = "UNLOAD"
+    EXTRA = "EXTRA"

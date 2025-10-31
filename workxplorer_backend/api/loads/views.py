7c213a919603ad04ec2a6e1272c35483abd09de0
Aleksandr <chatyev@inbox.ru>
2025-10-31 10:06:48 +0500

    fix import

--- workxplorer_backend/api/loads/views.py
+++ workxplorer_backend/api/loads/views.py
@@ -2,7 +2,7 @@ from django.contrib.gis.db.models.functions import Distance
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
 from django.core.exceptions import ValidationError
-from django.db.models import Count, DecimalField, ExpressionWrapper, F, FloatField, Q, Func
+from django.db.models import Count, DecimalField, ExpressionWrapper, F, FloatField, Func, Q
 from django.db.models.functions import Coalesce
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
--- workxplorer_backend/api/ratings/views.py
+++ workxplorer_backend/api/ratings/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from rest_framework import permissions, viewsets
+
 from .models import UserRating
 from .serializers import UserRatingSerializer
 

3b0c58e4aa9a46e512eeb59b1696f0b6fee39cc0
Aleksandr <chatyev@inbox.ru>
2025-11-21 18:45:50 +0500

    jwt token

--- workxplorer_backend/core/settings/base.py
+++ workxplorer_backend/core/settings/base.py
@@ -187,7 +187,7 @@ SPECTACULAR_SETTINGS = {
 
 # JWT
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=int(getenv("JWT_ACCESS_MIN", "30"))),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=int(getenv("JWT_ACCESS_MIN", "60"))),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=int(getenv("JWT_REFRESH_DAYS", "7"))),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,

f7cd73d6642c399507ee0637c4aa64a441917415
Aleksandr <chatyev@inbox.ru>
2025-11-27 18:46:29 +0500

    fix asgi

--- workxplorer_backend/core/asgi.py
+++ workxplorer_backend/core/asgi.py
@@ -2,22 +2,18 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.prod")
+
+import django
 
 django.setup()
 
-
-def get_websocket_urlpatterns():
-    from api.notifications.routing import websocket_urlpatterns
-
-    return websocket_urlpatterns
-
+from api.notifications.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(URLRouter(get_websocket_urlpatterns())),
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
     }
 )

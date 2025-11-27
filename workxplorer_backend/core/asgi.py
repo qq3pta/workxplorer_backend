8109717b7a6333be61242592d4cf7820a947f2c3
Aleksandr <chatyev@inbox.ru>
2025-11-27 19:06:18 +0500

    ruff asgi

--- workxplorer_backend/core/asgi.py
+++ workxplorer_backend/core/asgi.py
@@ -1,20 +1,14 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.prod")
-
-
-import django
-
-django.setup()
-
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-
 from api.notifications.middleware import JwtAuthMiddleware
 from api.notifications.routing import websocket_urlpatterns
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.prod")
 
 
 application = ProtocolTypeRouter(

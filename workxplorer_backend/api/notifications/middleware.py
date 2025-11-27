009df0de6a380647c7187397064e31c5eff073d7
Aleksandr <chatyev@inbox.ru>
2025-11-27 19:10:19 +0500

    fix moddleware

--- workxplorer_backend/api/notifications/middleware.py
+++ workxplorer_backend/api/notifications/middleware.py
@@ -1,45 +1,31 @@
 import jwt
 from django.conf import settings
-from channels.db import database_sync_to_async
-from channels.middleware import BaseMiddleware
 from django.contrib.auth import get_user_model
+from urllib.parse import parse_qs
 
 
-User = get_user_model()
-
-
-@database_sync_to_async
-def get_user(user_id):
-    try:
-        return User.objects.get(id=user_id)
-    except User.DoesNotExist:
-        return None
-
-
-class JwtAuthMiddleware(BaseMiddleware):
+class JwtAuthMiddleware:
     """
-    Custom WebSocket middleware to authenticate users using JWT tokens.
+    Custom JWT auth for WebSockets.
     """
 
-    async def __call__(self, scope, receive, send):
-        query_string = scope.get("query_string", b"").decode()
-        token = None
+    def __init__(self, inner):
+        self.inner = inner
 
-        if "token=" in query_string:
-            token = query_string.split("token=")[-1]
+    def __call__(self, scope):
+        User = get_user_model()
+
+        query = parse_qs(scope["query_string"].decode())
+        token = query.get("token", [None])[0]
 
         scope["user"] = None
 
         if token:
             try:
-                payload = jwt.decode(
-                    token,
-                    settings.SECRET_KEY,
-                    algorithms=["HS256"],
-                )
-                user = await get_user(payload.get("user_id"))
-                scope["user"] = user
+                payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+                user_id = payload.get("user_id")
+                scope["user"] = User.objects.get(id=user_id)
             except Exception:
                 scope["user"] = None
 
-        return await super().__call__(scope, receive, send)
+        return self.inner(scope)

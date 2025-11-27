b09a51f95659955a8a25b268d6111ab7e1f5be76
Aleksandr <chatyev@inbox.ru>
2025-11-27 21:24:53 +0500

    fix asgi re

--- workxplorer_backend/core/asgi.py
+++ workxplorer_backend/core/asgi.py
@@ -1,23 +1,25 @@
 import os
 import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 
-# 1. Настраиваем Django ещё до любых импортов
+# --------------------------------------------------------------
+# Настройка Django ДО любых импортов из Django
+# --------------------------------------------------------------
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.prod")
-
-# 2. Запуск Django
 django.setup()
 
-# 3. Только теперь можно импортировать всё что связано с Django
-from api.notifications.middleware import JwtAuthMiddleware
-from api.notifications.routing import websocket_urlpatterns
+# --------------------------------------------------------------
+# Теперь можно импортировать зависимости Django
+# --------------------------------------------------------------
+from api.notifications.middleware import JwtAuthMiddleware  # noqa: E402
+from api.notifications.routing import websocket_urlpatterns  # noqa: E402
 
-# 4. HTTP → обычное django ASGI-приложение
+# --------------------------------------------------------------
+# ASGI-приложение
+# --------------------------------------------------------------
 django_asgi_app = get_asgi_application()
 
-# 5. Финальная конфигурация channels
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,

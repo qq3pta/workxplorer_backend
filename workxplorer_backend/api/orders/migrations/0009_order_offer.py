071a5e9099749b7b56d7eb3b4b612c4e0f0ec554
Aleksandr <chatyev@inbox.ru>
2025-11-29 17:56:39 +0500

    update ruff

--- workxplorer_backend/api/orders/migrations/0009_order_offer.py
+++ workxplorer_backend/api/orders/migrations/0009_order_offer.py
@@ -5,16 +5,21 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('offers', '0004_alter_offer_price_value'),
-        ('orders', '0008_remove_orderdocument_orderdoc_order_idx_and_more'),
+        ("offers", "0004_alter_offer_price_value"),
+        ("orders", "0008_remove_orderdocument_orderdoc_order_idx_and_more"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='order',
-            name='offer',
-            field=models.OneToOneField(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='order', to='offers.offer'),
+            model_name="order",
+            name="offer",
+            field=models.OneToOneField(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="order",
+                to="offers.offer",
+            ),
         ),
     ]

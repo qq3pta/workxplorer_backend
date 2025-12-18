ca8e0f659fbb981c1cecb1f33f23b1375000d7ca
Aleksandr <chatyev@inbox.ru>
2025-12-18 22:17:35 +0500

    fix import

--- workxplorer_backend/api/payments/views.py
+++ workxplorer_backend/api/payments/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 
 from .models import Payment
 from .serializers import PaymentCreateSerializer, PaymentSerializer

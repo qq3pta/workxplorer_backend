edbf2384c32b079f34a1b3f6773a725692c22df3
Aleksandr <chatyev@inbox.ru>
2025-12-23 14:17:45 +0500

    add logs

--- workxplorer_backend/api/offers/urls.py
+++ workxplorer_backend/api/offers/urls.py
@@ -1,10 +1,19 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import OfferViewSet
+from .views import OfferViewSet, OfferStatusLogListView
 
 app_name = "offers"
 
 router = DefaultRouter()
 router.register(r"", OfferViewSet, basename="offers")
 
-urlpatterns = router.urls
+urlpatterns = [
+    *router.urls,
+    # 🔹 ЛОГИ ОФФЕРА
+    path(
+        "<int:pk>/logs/",
+        OfferStatusLogListView.as_view(),
+        name="offer-logs",
+    ),
+]

513aac66a2a6b06c1375d79c65c7b2bccf247726
Aleksandr <chatyev@inbox.ru>
2025-12-26 10:48:54 +0500

    radius update

--- workxplorer_backend/api/loads/views.py
+++ workxplorer_backend/api/loads/views.py
@@ -320,13 +320,13 @@ class PublicLoadsView(generics.ListAPIView):
 
         p = self.request.query_params
 
-        o_lat = p.get("origin_lat")
-        o_lng = p.get("origin_lng")
-        o_r = p.get("origin_radius_km")
+        # o_lat = p.get("origin_lat")
+        # o_lng = p.get("origin_lng")
+        # o_r = p.get("origin_radius_km")
 
-        d_lat = p.get("dest_lat")
-        d_lng = p.get("dest_lng")
-        d_r = p.get("dest_radius_km")
+        # d_lat = p.get("dest_lat")
+        # d_lng = p.get("dest_lng")
+        # d_r = p.get("dest_radius_km")
 
         if p.get("uuid"):
             qs = qs.filter(uuid=p["uuid"])
@@ -377,21 +377,10 @@ class PublicLoadsView(generics.ListAPIView):
                 Q(customer__phone__icontains=p["customer_phone"])
                 | Q(customer__phone_number__icontains=p["customer_phone"])
             )
-        # ---------- GEO ANNOTATIONS ----------
-        if o_lat and o_lng:
-            origin_user_point = Point(float(o_lng), float(o_lat), srid=4326)
-            qs = qs.annotate(origin_dist_km=Distance("origin_point", origin_user_point) / 1000.0)
-
-        if d_lat and d_lng:
-            dest_user_point = Point(float(d_lng), float(d_lat), srid=4326)
-            qs = qs.annotate(dest_dist_km=Distance("dest_point", dest_user_point) / 1000.0)
-
-        # ---------- GEO FILTERS ----------
-        if o_lat and o_lng and o_r:
-            qs = qs.filter(origin_dist_km__lte=float(o_r))
-
-        if d_lat and d_lng and d_r:
-            qs = qs.filter(dest_dist_km__lte=float(d_r))
+            # ---------- ROUTE LENGTH FILTER (ГЛАВНОЕ) ----------
+        max_km = p.get("origin_radius_km")
+        if max_km:
+            qs = qs.filter(route_km__lte=float(max_km))
 
         allowed = {
             "path_km",

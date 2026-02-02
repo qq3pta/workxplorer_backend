083ce687194debe601ec7c3c22c4f6c195075fd1
Aleksandr <chatyev@inbox.ru>
2026-02-02 17:51:55 +0500

    fix routing

--- workxplorer_backend/api/loads/routing.py
+++ workxplorer_backend/api/loads/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from .consumers import CargoConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/loads/$", CargoConsumer.as_asgi()),
+    re_path(r"^ws/loads/?$", CargoConsumer.as_asgi()),
 ]

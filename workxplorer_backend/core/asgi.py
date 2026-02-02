9ffcf99b2690d69c03d5cf57b055962e8530606e
Aleksandr <chatyev@inbox.ru>
2026-02-02 16:51:16 +0500

    update asgi

--- workxplorer_backend/core/asgi.py
+++ workxplorer_backend/core/asgi.py
@@ -1,29 +1,27 @@
 import os
-
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-# --------------------------------------------------------------
-# Настройка Django ДО любых импортов из Django
-# --------------------------------------------------------------
+# 1. Настройка Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.prod")
 django.setup()
 
-# --------------------------------------------------------------
-#
-# --------------------------------------------------------------
-from api.notifications.middleware import JwtAuthMiddleware  # noqa: E402
-from api.notifications.routing import websocket_urlpatterns  # noqa: E402
+# 2. Импорты роутинга из ВСЕХ приложений
+from api.notifications.middleware import JwtAuthMiddleware
+from api.notifications.routing import websocket_urlpatterns as notifications_ws
+from api.loads.routing import websocket_urlpatterns as loads_ws  # Тот самый файл для карго
 
-# --------------------------------------------------------------
-# ASGI-приложение
-# --------------------------------------------------------------
+# 3. Объединяем списки путей в один
+combined_urlpatterns = notifications_ws + loads_ws
+
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": JwtAuthMiddleware(URLRouter(websocket_urlpatterns)),
+        "websocket": JwtAuthMiddleware(
+            URLRouter(combined_urlpatterns)  # Используем объединенный список
+        ),
     }
 )

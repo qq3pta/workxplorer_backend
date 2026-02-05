d9d910821d1047ba5d3053f6744b14f7105e65ec
Aleksandr <chatyev@inbox.ru>
2026-02-05 11:29:45 +0500

    update time last seen

--- workxplorer_backend/common/middleware.py
+++ workxplorer_backend/common/middleware.py
@@ -1,10 +1,14 @@
 from django.utils import timezone
+from datetime import timedelta
 
 
 class LastSeenMiddleware:
     """
-    Обновляет last_seen при любом HTTP запросе пользователя
+    Обновляет last_seen, но не чаще чем раз в 60 секунд
+    (чтобы не грузить БД)
     """
+
+    UPDATE_INTERVAL = timedelta(seconds=60)
 
     def __init__(self, get_response):
         self.get_response = get_response
@@ -13,7 +17,10 @@ class LastSeenMiddleware:
         user = getattr(request, "user", None)
 
         if user and user.is_authenticated:
-            user.last_seen = timezone.now()
-            user.save(update_fields=["last_seen"])
+            now = timezone.now()
+
+            if not user.last_seen or (now - user.last_seen) > self.UPDATE_INTERVAL:
+                user.last_seen = now
+                user.save(update_fields=["last_seen"])
 
         return self.get_response(request)

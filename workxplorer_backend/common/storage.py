adf3816fbeaaf84d8372cc9eaf75ff0fb86f2ef5
Aleksandr <chatyev@inbox.ru>
2026-04-30 16:43:24 +0500

    fix storages

--- workxplorer_backend/common/storage.py
+++ workxplorer_backend/common/storage.py
@@ -1,20 +1,18 @@
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage, Storage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
 def _build_s3_storage() -> Storage:
-    from storages.backends.s3 import S3Storage
-
-    return S3Storage(
+    return S3Boto3Storage(
+        access_key=settings.AWS_ACCESS_KEY_ID,
+        secret_key=settings.AWS_SECRET_ACCESS_KEY,
         bucket_name=settings.AWS_STORAGE_BUCKET_NAME,
         endpoint_url=settings.AWS_S3_ENDPOINT_URL,
         region_name=settings.AWS_S3_REGION_NAME,
-        access_key=settings.AWS_S3_ACCESS_KEY_ID,
-        secret_key=settings.AWS_S3_SECRET_ACCESS_KEY,
-        default_acl="public-read",
+        default_acl=None,
         querystring_auth=False,
         file_overwrite=False,
-        addressing_style="virtual",
     )
 
 

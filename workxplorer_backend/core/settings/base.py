813aeecbbbc3cdd91e3f6c8e2d2d93fa7bc3cce6
SHXDev <geoman1127@gmail.com>
2026-05-21 17:46:54 +0500

    Revert "Продление JWT токена (access = 8h, remember=24h)"
    This reverts commit 2a8a21ce0b42b4b61cdee03f405309212974c9ea.

--- workxplorer_backend/core/settings/base.py
+++ workxplorer_backend/core/settings/base.py
@@ -223,7 +223,7 @@ SPECTACULAR_SETTINGS = {
 
 # JWT
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=int(getenv("JWT_ACCESS_MIN", "480"))),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=int(getenv("JWT_ACCESS_MIN", "60"))),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=int(getenv("JWT_REFRESH_DAYS", "7"))),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
